d78751cdad325aa7c9aacc0b3b439cd34280be8e
saaakh63 <sara.khalefa708@compit.aun.edu.eg>
2025-08-04 01:31:16 +0300

    fix frontend

--- interface/src/app/app.routes.ts
+++ interface/src/app/app.routes.ts
@@ -5,15 +5,14 @@ import {
 } from '@angular/router';
 
 import { HomeComponent } from './Components/home/home.component';
+import {
+  LeaderboardComponent,
+} from './Components/leaderboard/leaderboard.component';
 import { LoginComponent } from './Components/login/login.component';
-<<<<<<< HEAD
 import { MapComponent } from './Components/map/map.component';
 import { PlayComponent } from './Components/play/play.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { SiwaComponent } from './Components/siwa/siwa.component';
-=======
-import { LeaderboardComponent } from './Components/leaderboard/leaderboard.component';
->>>>>>> 4c39f1a88de821aaebabd9e76724891357ea4cb4
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -22,7 +21,8 @@ export const routes: Routes = [
   {path:'map', component:MapComponent},
   {path:'register', component:RegisterComponent},
   {path:'login', component:LoginComponent},
-  {path:'leaderboard', component:LeaderboardComponent}
+  {path:'leaderboard', component:LeaderboardComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
