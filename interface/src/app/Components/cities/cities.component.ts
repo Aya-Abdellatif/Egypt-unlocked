ad3a6582d539f889c18025aaadfd7ba242a24f84
saaakh63 <sara.khalefa708@compit.aun.edu.eg>
2025-08-03 13:50:16 +0300

    add cities

---
+++ interface/src/app/Components/cities/cities.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cities',
+  imports: [],
+  standalone: true,
+  templateUrl: './cities.component.html',
+  styleUrls: ['./cities.component.css']
+})
+export class CitiesComponent {
+
+}
--- interface/src/app/app.component.ts
+++ interface/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { CitiesComponent } from './Components/cities/cities.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CitiesComponent, HomeComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'interface';
