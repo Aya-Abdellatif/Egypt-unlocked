d29b8487d725c8a69c36b77d213d4005a7e50a38
saaakh63 <sara.khalefa708@compit.aun.edu.eg>
2025-08-03 15:58:02 +0300

    cities

---
+++ interface/src/app/Components/siwa/siwa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-siwa',
+  imports: [],
+  templateUrl: './siwa.component.html',
+  styleUrl: './siwa.component.css'
+})
+export class SiwaComponent {
+
+}
