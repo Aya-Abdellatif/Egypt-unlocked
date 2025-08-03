fe486aab105c0a40de3d13d6c3f4bf1e93745427
saaakh63 <sara.khalefa708@compit.aun.edu.eg>
2025-08-04 00:35:12 +0300

    auth

--- interface/src/app/Components/navbar/navbar.component.ts
+++ interface/src/app/Components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {
+  RouterLink,
+  RouterLinkActive,
+  RouterModule,
+} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
 }
+
