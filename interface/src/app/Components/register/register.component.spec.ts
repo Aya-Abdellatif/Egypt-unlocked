e36d60a9e97637f89ee85dfa5fa706f4e70955a3
saaakh63 <sara.khalefa708@compit.aun.edu.eg>
2025-08-03 23:55:23 +0300

    auth

---
+++ interface/src/app/Components/register/register.component.spec.ts
@@ -0,0 +1,30 @@
+// register.component.ts
+import { Component } from '@angular/core';
+
+import { AuthService } from '../../../Service/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  username: string = '';
+  password: string = '';
+Model: any;
+
+  constructor(private authService: AuthService) { }
+
+  onRegister(): void {
+    this.authService.register(this.username, this.password).subscribe(
+      response => {
+        console.log('Registration successful', response);
+        // Handle successful registration (e.g., navigate to login page)
+      },
+      error => {
+        console.error('Registration failed', error);
+        // Handle registration error (e.g., show error message)
+      }
+    );
+  }
+}
