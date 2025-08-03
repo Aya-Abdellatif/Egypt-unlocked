79ac4029101c130a88cac631437b1faff1f167d6
Aya Abdellatif <ayaabdllatif112@gmail.com>
2025-08-03 20:29:11 +0300

    map

--- interface/src/app/app.routes.ts
+++ interface/src/app/app.routes.ts
@@ -7,11 +7,13 @@ import {
 import { HomeComponent } from './Components/home/home.component';
 import { PlayComponent } from './Components/play/play.component';
 import { SiwaComponent } from './Components/siwa/siwa.component';
+import { MapComponent } from './Components/map/map.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'play', component: PlayComponent },
   {path:'siwa',component:SiwaComponent},
+  {path:'map', component:MapComponent}
 ];
 
 @NgModule({
